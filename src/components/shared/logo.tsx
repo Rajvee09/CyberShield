180cbd69ddd882eff4c7bd625c82758ddccd4c49
Madhav Aggarwal <madhavaggarwal4016@gmail.com>
2025-09-16 08:07:12 +0000

    now change the logo to something new logo which consist of only initials

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -12,16 +12,8 @@ export function Logo(props: React.SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z" />
-      <path d="M12 10.5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z" />
-      <path d="M12 15.5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z" />
-      <path d="M12 20.5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z" />
-      <path d="m4.5 13 3-2.5" />
-      <path d="m19.5 13-3-2.5" />
-      <path d="m4.5 18 3-2.5" />
-      <path d="m19.5 18-3-2.5" />
-      <path d="m14.5 13 3-2.5" />
-      <path d="m9.5 8-3-2.5" />
+      <path d="M15 12h-2a4 4 0 1 0 0-8h2" />
+      <path d="M18 20a4 4 0 1 1-4-4h2a4 4 0 1 0 0-8h-2" />
     </svg>
   );
 }
