0eaee8e0e398e4cc2ec0b4eb5a8f6add63c8cf89
Madhav Aggarwal <madhavaggarwal4016@gmail.com>
2025-09-16 08:07:51 +0000

    invert the logo

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -5,8 +5,8 @@ export function Logo(props: React.SVGProps<SVGSVGElement>) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
+      fill="currentColor"
+      stroke="none"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
